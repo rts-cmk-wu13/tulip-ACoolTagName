70c13c9e868fdba1c818baa276c6ab48af952c37
giaki <grigoriosvousios@gmail.com>
2025-06-12 13:13:21 +0200

    going good

---
+++ htmljs/leyout.js
@@ -0,0 +1,7 @@
+const wrapped = document.getElementById('wrapper');
+
+wrapped.innerHTML = `
+<header id="header"></header>
+<main></main>
+<footer id="footer"></footer>
+`;
---
+++ htmljs/meatNpotatos.js
@@ -0,0 +1,83 @@
+/*const meat = document.querySelector("main");
+
+meat.innerHTML = `
+<section class="page_section"></section>
+    <article>
+        <section class="article_header">
+            <header>
+                <hgroup>
+                    <h2></h2>
+                    <p></p>
+                </hgroup>
+            </header>
+        </section>
+        <section class="article_content"></section>
+    </article>
+</section>
+<section class="page_section">
+    <article>
+        <section class="article_header">
+            <header>
+                <hgroup>
+                    <h2></h2>
+                    <p></p>
+                </hgroup>
+            </header>
+        </section>
+        <section class="article_content"></section>
+    </article>
+</section>
+<section class="page_section">
+    <article>
+        <section class="article_header">
+            <header>
+                <hgroup>
+                    <h2></h2>
+                    <p></p>
+                </hgroup>
+            </header>
+        </section>
+        <section class="article_content"></section>
+    </article>
+</section>
+<section class="page_section">
+    <article>
+        <section class="article_header">
+            <header>
+                <hgroup>
+                    <h2></h2>
+                    <p></p>
+                </hgroup>
+            </header>
+        </section>
+        <section class="article_content"></section>
+    </article>
+</section>
+`
+*/
+
+const meat = document.querySelector("main");
+
+const content = document.getElementById("page_section");
+
+
+meat.innerHTML = `
+<section class="page_section"></section></section>
+<section class="page_section"></section></section>
+<section class="page_section"></section></section>
+<section class="page_section"></section></section>
+`
+
+meat.innerHTML = `
+    <article>
+        <section class="article_header">
+            <header>
+                <hgroup>
+                    <h2></h2>
+                    <p></p>
+                </hgroup>
+            </header>
+        </section>
+        <section class="article_content"></section>
+    </article>
+`
