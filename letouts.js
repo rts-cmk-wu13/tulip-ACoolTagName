2f0fd5e554ce22fa2d1d5df907c76b3b0cd810b3
giaki <grigoriosvousios@gmail.com>
2025-06-19 14:02:09 +0200

    as good for to day

--- letouts.js
+++ letouts.js
@@ -5,6 +5,19 @@ fetch('letouts.json')
     document.getElementById('header').innerHTML = data.header;
     document.querySelector('main').innerHTML = data.page_section;
     
+    document.getElementById('fn').innerHTML = data.name.first;
+    document.getElementById('mn').innerHTML = data.name.middle;
+    document.getElementById('ln').innerHTML = data.name.last;
+    document.getElementById('nt').innerHTML = data.name.title;
 
   })
   .catch(error => console.error(error));
+
+fetch('http://localhost:4000/hero')
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('headline').innerHTML = data.headline;
+  })
+  .catch(error => console.error(error));
+
+
