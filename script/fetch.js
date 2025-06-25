cc970404679a5b0d5b7cb6ef304f608f2416af89
giaki <grigoriosvousios@gmail.com>
2025-06-25 14:37:38 +0200

    suverant

--- script/fetch.js
+++ script/fetch.js
@@ -23,32 +23,53 @@ fetch('http://localhost:4000/rooms')
 
 
 
-fetch('http://localhost:4000/spots')
+
+
+
+
+fetch('http://localhost:4000/services')
     .then(response => response.json())
     .then(data => {
 
         document.getElementById('services_headline-h').innerHTML = data.headline;
         document.getElementById('services_headline-p').innerHTML = data.text;
-        const servicesCIA = document.getElementById('services_content-info_area');
-        const servicesCCA = document.getElementById('services_content-cards_area');
-        
+
+        const servicesCInfoArea = document.getElementById('services_content-info_area');
+
+        data.facilities.map((element) => {
+
+            const sectionInfo = document.createElement('section'); servicesCInfoArea.append(sectionInfo);
+            const infoDiv = document.createElement('div'); sectionInfo.append(infoDiv)
+            const h2 = document.createElement('h2'); infoDiv.append(h2); h2.innerHTML = element.name;
+            const p1 = document.createElement('p'); infoDiv.append(p1); p1.innerHTML = element.text1;
+            const p2 = document.createElement('p'); infoDiv.append(p2); p2.innerHTML = element.text2;
+
+        })
+
+
+
+    })
+    .catch(error => console.error(error));
+
+
+
+
+fetch('http://localhost:4000/spots')
+    .then(response => response.json())
+    .then(data => {
+
+        const servicesCCardArea = document.getElementById('services_content-cards_area');
+
         data.map((element) => {
 
+            const sectionCards = document.createElement('section'); servicesCCardArea.append(sectionCards)
+            const img = document.createElement('img'); sectionCards.append(img); img.src = element.icon;
+            const h2 = document.createElement('h2'); sectionCards.append(h2); h2.innerHTML = element.name;
+            const p = document.createElement('p'); sectionCards.append(p); p.innerHTML = element.text;
 
-            const sectionI = document.createElement('section'); servicesCIA.append(sectionI)
 
 
 
-
-
-            const sectionC = document.createElement('section'); servicesCCA.append(sectionC)
-            const img = document.createElement('img'); sectionC.append(img);
-            const h2 = document.createElement('h2'); sectionC.append(h2); 
-            const p = document.createElement('p'); sectionC.append(p);
-            console.log(sectionC);
-            img.src = element.icon
-            h2.innerHTML = element.name
-            p.innerHTML = element.text
 
         })
 
